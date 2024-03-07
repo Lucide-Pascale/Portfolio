851f40ac7bb518bb8665750146392cb80fbbad67
“mugishaj092” <mugishajoseph092@gmail.com>
2024-03-07 15:05:17 +0200

    single Blog

--- Script/script.js
+++ Script/script.js
@@ -58,3 +58,53 @@ function updateCopyright() {
   }
 }
 window.onload = updateCopyright;
+
+
+document.getElementById('downloadButton').addEventListener('click', function() {
+    var downloadLink = document.createElement('a');
+    downloadLink.setAttribute('href', 'assets/resume.pdf');
+    downloadLink.setAttribute('download', 'resume.pdf');
+    downloadLink.click();
+});
+
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+
+function postComment() {
+  var commentInput = document.getElementById('editor');
+  var namesInput = document.getElementById('names');
+  
+  var commentList = document.getElementById('comments-container');
+  var commentText = commentInput.value;
+  commentInput.value = '';
+  console.log(commentText);
+
+  var newComment = document.createElement('li');
+  newComment.innerHTML = `    <div id="comments-container">
+  <div id="comment-item" style="background-color: var(--back-color); border-radius: 2px; padding: 5px; display: flex; align-items: center; width: 30%; margin: 10px 0px;">
+      <div class="imagec" style="width: 60px;">
+          <img src="./Assets/profile.JPG" alt="" style="width: 100%; border-radius: 50%;">
+      </div>
+      <div class="comment-content">
+          <div class="div" style="display: flex; justify-content: space-between;">
+              <p style="margin: 3px; font-size: .8rem;">MUGISHA Joseph</p>
+              <p style="margin: 3px; font-size: .6rem;">12:00pm 1 jan 2024</p>
+          </div>
+          <p style="margin: 3px; font-size: .7rem;">${newComment}</p>
+      </div>
+  </div>`;
+  commentList.appendChild(newComment);
+}
+
+
+
